b61d2c2400fd641798802959936e8515091a3472
VToropov <vladimirtoropov@MacBook-Air-Vladimir.local>
2017-01-21 16:02:30 +0300

    new safe

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+require 'sinatra/activerecord'
+# создаем подключеие к базе данных! Надо запомнить
+set :database, "sqlite3:barbershop.db"
+
+
+
+class Client < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
